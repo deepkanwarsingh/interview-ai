7c73434e4af6d00dccaa2e464c3f6a000205d314
Chamanjeet-coder <chamanjeets@gmail.com>
2026-06-03 00:23:58 +0530

    PORT IN ENV

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,6 @@ connectDB();//connect to Db
 // Call Gemini AI function
 // invokegeminiAi();
 
-app.listen(3000, () => {
+app.listen(process.env.port || 3000, () => {
     console.log("server is running on port 3000");
 });
