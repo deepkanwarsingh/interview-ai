ac56eb6209d2865d4cfe2e9112f1143c85aa5ebb
Chamanjeet-coder <chamanjeets@gmail.com>
2026-06-03 20:30:32 +0530

    token validation via header

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -2,16 +2,19 @@ import jwt from "jsonwebtoken"
 
 
 function authMiddleware(req, res, next) {
-    const token = req.cookies.token
-
+    const token = req.headers.token.split(" ")
     if (!token) {
         return res.status(401).json({ message: "Token not provided" })
+    }
+    // console.log(token[0]);
+    if(token[0]!="Bearer"){
+        return res.status(401).json({ message: "Token provided is wrong" })
     }
 
     // jwt.verify(token,process.env.JWT_SECRET)
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const decoded = jwt.verify(token[1], process.env.JWT_SECRET)
 
         req.user = decoded
 
